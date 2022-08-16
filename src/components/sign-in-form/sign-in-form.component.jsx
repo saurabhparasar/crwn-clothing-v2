e4bdc8c527810433ff101b4f089298dea1568465
saurabhparasar <saurabhparasar3010@gmail.com>
2022-08-16 22:04:18 +0530

    create context start

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import "./sign-in-form.styles.scss";
 import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
+import { UserContext } from "../../contexts/user.contexts";
 import {
   createUserDocumentFromAuth,
   signInWithGooglePopup,
@@ -33,11 +34,10 @@ const SignInForm = () => {
     e.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
+      const { user } = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-      console.log(response);
     } catch (error) {
       if (error.code == "auth/wrong-password") {
         alert("incorrect password for email");
